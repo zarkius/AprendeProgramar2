3d891f5d376124c7040ab515fc732b90fea66640
zarkius <diegomallor2.0@gmail.com>
2024-12-23 06:55:14 +0100

    a

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,51 +1,38 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+
     alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.google.firebase.crashlytics)
     alias(libs.plugins.google.gms.google.services) apply false
-    alias(libs.plugins.google.firebase.firebase.perf)
 }
 
 android {
-    namespace = "com.playconsole.AprendeProgramar2"
-    compileSdk = 35
+    namespace = "com.playconsole.aprendeprogramar2"
+    compileSdk = 34
     defaultConfig {
         minSdk = 26
-        versionCode = 2
+        versionCode = 1
         versionName = "1.0"
     }
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            signingConfig = signingConfigs.getByName("debug")
-        }
-    }
-    buildFeatures {
-        compose = true
-        }
-    buildToolsVersion = "35.0.0"
-    ndkVersion = "28.0.12674087 rc2"
-    dependenciesInfo {
-        includeInApk = false
-        includeInBundle = false
-    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
 }
+
 repositories {
     google()
     mavenCentral()
-    gradlePluginPortal()
 }
+
+
+
 dependencies {
     implementation(libs.play.services.ads)
     implementation(libs.play.services.maps)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.runtime)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)// Example within the Firebase group:
     implementation(libs.firebase.analytics)
     implementation(libs.firebase.crashlytics)
     implementation(libs.firebase.database)
