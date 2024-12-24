2a3420e92e068e95d2b648e11826b013a5a58887
zarkius <diegomallor2.0@gmail.com>
2024-12-24 13:13:27 +0100

    reinit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.aprendeprogramar2"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.aprendeprogramar2"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
